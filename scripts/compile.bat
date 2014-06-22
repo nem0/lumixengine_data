d0c92ee2239b074b6769a284435d19a7c601f814
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-06-22 13:30:42 +0200

    tree shader in birch* materials; compile.bat uses the right path on both x86 and x64 windows

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -1,6 +1,10 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+	call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+) else (
+	call "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+)
 set dll_name=%1
 set dll_name=%dll_name:.cpp=.obj%
 cd scripts
-cl.exe /LDd /DEBUG /I"F:\Projects\LuxEngine\src" "%~nx1" /link /DYNAMICBASE "kernel32.lib" "user32.lib" "core.lib" "engine.lib"  /DEBUG /LIBPATH:"F:\Projects\LuxEngine\bin\Win32_Debug"
-exit %errorlevel%
+cl.exe /LDd /DEBUG /I"D:\Projects\LuxEngine\src" "%1" /link /DYNAMICBASE "kernel32.lib" "user32.lib" "core.lib" "engine.lib"  /DEBUG /LIBPATH:"D:\Projects\LuxEngine\bin\Win32_Debug"
+REM exit %errorlevel%

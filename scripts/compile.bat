1c6622439225241ac4440a808b8ecf5b25904892
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-07-16 20:20:19 +0200

    new executables

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -6,5 +6,5 @@ if exist "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" 
 set dll_name=%1
 set dll_name=%dll_name:.cpp=.obj%
 cd scripts
-cl.exe /LDd /DEBUG /I"D:\Projects\LumixEngine\src" "%1" /link /DYNAMICBASE "kernel32.lib" "user32.lib" "core.lib" "engine.lib"  /DEBUG /LIBPATH:"D:\Projects\LumixEngine\bin\Win32_Debug"
+cl.exe /LDd /DEBUG /I"D:\Projects\LumixEngine\src" "%1" /link /DYNAMICBASE "kernel32.lib" "user32.lib" "core.lib" "engine.lib" "physics.lib"  /DEBUG /LIBPATH:"D:\Projects\LumixEngine\bin\Win32_Release"
 exit %errorlevel%
--- scripts/e2.cpp
+++ scripts/e2.cpp
@@ -9,6 +9,7 @@
 #include <cstdio>
 #include "graphics/renderer.h"
 #include "universe/universe.h"
+#include "physics/physics_scene.h"
 
 #define DLL_EXPORT  __declspec(dllexport)
 #include <Windows.h>
@@ -20,47 +21,16 @@ class MyScript : public Lumix::BaseScript
 		virtual void create(Lumix::ScriptSystem& ctx, Lumix::Entity entity) override
 		{
 			m_e = entity;
-			/*Lumix::Component animable = m_e.getComponent(crc32("animable"));
-			if(animable.isValid())
-			{
-				Lumix::AnimationSystem* sys = static_cast<Lumix::AnimationSystem*>(animable.system);
-				sys->playAnimation(animable, "models/horse.ani");
-			}*/
+			m_phy_controller = entity.getComponent(crc32("physical_controller"));
+			m_phy_scene = static_cast<Lumix::PhysicsScene*>(m_phy_controller.system);
 		}
 
 		virtual void update(float dt) override
 		{
-			if(GetAsyncKeyState(VK_UP) >> 8)
-				m_e.setPosition(m_e.getPosition() + Lumix::Vec3(0, 0, -10 * dt));
-			if(GetAsyncKeyState(VK_DOWN) >> 8)
-				m_e.setPosition(m_e.getPosition() + Lumix::Vec3(0, 0, 10 * dt));
-			//Lumix::Component renderable = m_e.getComponent(crc32("renderable"))
-			/*Lumix::Vec3 pos;
-			static_cast<Lumix::Renderer*>(m_renderables[0].system)->getBonePosition(m_renderables[0], "HRigPelvis", &pos);
-			Lumix::Matrix mtx;
-			Lumix::Matrix::setIdentity(mtx);
-			
-			Lumix::Vec3 v(pos.x, pos.y - 10, pos.z);
-			v.normalize();
-			mtx.m31 = -v.x;
-			mtx.m32 = -v.y;
-			mtx.m33 = -v.z;
-		
-			Lumix::Vec3 up(0, 1, 0); 
-			Lumix::Vec3 right = Lumix::crossProduct(v, up);
-			up = Lumix::crossProduct(right, v);
-
-			mtx.m21 = up.x;
-			mtx.m22 = up.y;
-			mtx.m23 = up.z;
-
-			mtx.m11 = right.x;
-			mtx.m12 = right.y;
-			mtx.m13 = right.z;
-			mtx.setTranslation(Lumix::Vec3(0, 10, 0));
-
-			static_cast<Lumix::Renderer*>(m_renderables[0].system)->setCameraMatrix(mtx);
-			*/
+			if (GetAsyncKeyState(VK_UP) >> 8)
+				m_phy_scene->moveController(m_phy_controller, Lumix::Vec3(0, 0, -0.01f), dt);
+			if (GetAsyncKeyState(VK_DOWN) >> 8)
+				m_phy_scene->moveController(m_phy_controller, Lumix::Vec3(0, 0, 0.01f), dt);
 		}
 
 		virtual void visit(Lumix::ScriptVisitor& visitor) override
@@ -68,6 +38,8 @@ class MyScript : public Lumix::BaseScript
 		}
 
 		Lumix::Entity m_e;
+		Lumix::Component m_phy_controller;
+		Lumix::PhysicsScene* m_phy_scene;
 };
 
 extern "C" DLL_EXPORT Lumix::BaseScript* createScript()

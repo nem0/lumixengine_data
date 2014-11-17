5a62d9f358c1c140db91cd63751a78ab198b3309
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-11-18 00:06:29 +0100

    compile.bat - another possible path for visual studio compiler

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -3,12 +3,17 @@ if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
 	call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 	echo "Using Visual Studio 2013"
 ) else ( 
-	if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
-		call "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-		echo "Using Visual Studio 2012"	
-	) else (
-		call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
-		echo "Using Visual Studio 2012"
+	if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+		call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+		echo "Using Visual Studio 2013"
+	) else ( 
+		if exist "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" (
+			call "C:\Program Files\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+			echo "Using Visual Studio 2012"	
+		) else (
+			call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+			echo "Using Visual Studio 2012"
+		)
 	)
 )
 set dll_name=%1

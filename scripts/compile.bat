b2b65143490d95517b78b47e4a02fd347de330a0
Mikulas Florek <mikulas.florek@gamedev.sk>
2014-11-29 00:00:51 +0100

    not pdb for scripts as it is locked by visual studio and that causes reload to not work

--- scripts/compile.bat
+++ scripts/compile.bat
@@ -19,5 +19,5 @@ if exist "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
 set dll_name=%1
 set dll_name=%dll_name:.cpp=.obj%
 cd scripts
-cl.exe /LDd /DEBUG /I"D:\Projects\LumixEngine\src" "%1" /link /DYNAMICBASE "kernel32.lib" "user32.lib" "core.lib" "engine.lib" "physics.lib"  /DEBUG /LIBPATH:"D:\Projects\LumixEngine\bin\Win32_Release"
+cl.exe /LDd /I"D:\Projects\LumixEngine\src" "%1" /link /DYNAMICBASE "kernel32.lib" "user32.lib" "core.lib" "engine.lib" "physics.lib"  /LIBPATH:"D:\Projects\LumixEngine\bin\Win32_Release"
 exit %errorlevel%

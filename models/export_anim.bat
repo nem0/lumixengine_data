bcda264296e5999dc0701af6a2b540f861773190
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-05-03 14:16:15 +0200

    update export batch file

--- models/export_anim.bat
+++ models/export_anim.bat
@@ -1,4 +1,8 @@
 set blend_name=%1
-set ani_name=%blend_name:.blend=.ani%
-set base_path=%2
-"C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" "%blend_name%" --background -P "%base_path%\exporters\ani_export.py" -- "%ani_name%"
+set ani_name=%2
+set base_path=%3
+if exist "C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" (
+	"C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" "%blend_name%" --background -P "%base_path%\exporters\ani_export.py" -- "%ani_name%"
+) else (
+	"C:\Program Files\Blender Foundation\Blender\blender.exe" "%blend_name%" --background -P "%base_path%\exporters\ani_export.py" -- "%ani_name%"
+)
--- models/export_mesh.bat
+++ models/export_mesh.bat
@@ -1,6 +1,6 @@
 set blend_name=%1
-set msh_name=%blend_name:.blend=.msh%
-set base_path=%2
+set msh_name=%2
+set base_path=%3
 if exist "C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" (
 	"C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" "%blend_name%" --background -P "%base_path%\exporters\msh_export.py" -- "%msh_name%"
 ) else (

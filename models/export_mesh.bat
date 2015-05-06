c30969a51a604015efa159d60143fa56ff938c0c
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-05-07 01:01:09 +0200

    removed inactive files

--- models/export_mesh.bat
+++
@@ -1,8 +0,0 @@
-set blend_name=%1
-set msh_name=%2
-set base_path=%3
-if exist "C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" (
-	"C:\Program Files (x86)\Blender Foundation\Blender\blender.exe" "%blend_name%" --background -P "%base_path%\exporters\msh_export.py" -- "%msh_name%"
-) else (
-	"C:\Program Files\Blender Foundation\Blender\blender.exe" "%blend_name%" --background -P "%base_path%\exporters\msh_export.py" -- "%msh_name%"
-)

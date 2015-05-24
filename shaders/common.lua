742e70aa2111def8cec59af691ee10299de80219
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-05-24 14:57:57 +0200

    updated terrain shader

--- shaders/common.lua
+++ shaders/common.lua
@@ -7,11 +7,46 @@ end
 
 common_fragment = createShaderSource([[
 
-	float getFogFactor(float fFogCoord, float fog_density) 
+	uniform float fog_density;
+	float getFogFactor(float fFogCoord) 
 	{ 
 		float fResult = exp(-pow(fog_density * fFogCoord, 2.0)); 
 		fResult = 1.0-clamp(fResult, 0.0, 1.0); 
 		return fResult;
+	}
+	
+	float texture2DCompare(sampler2D depths, vec2 uv, ivec2 offset, float compare)
+	{
+		float depth = textureOffset(depths, uv, offset).r;
+		return step(compare, depth);
+	}
+
+	float texture2DShadowLerp(sampler2D depths, vec2 size, vec2 uv, float compare)
+	{
+		vec2 texelSize = vec2(1.0)/size;
+		vec2 f = fract(uv*size+0.5);
+		vec2 centroidUV = floor(uv*size+0.5)/size;
+
+		float lb = texture2DCompare(depths, centroidUV, ivec2(0, 0), compare);
+		float lt = texture2DCompare(depths, centroidUV, ivec2(0, 1), compare);
+		float rb = texture2DCompare(depths, centroidUV, ivec2(1, 0), compare);
+		float rt = texture2DCompare(depths, centroidUV, ivec2(1, 1), compare);
+		float a = mix(lb, lt, f.y);
+		float b = mix(rb, rt, f.y);
+		float c = mix(a, b, f.x);
+		return c;
+	}
+	
+	float PCF(sampler2D depths, vec2 size, vec2 uv, float compare)
+	{
+		float result = 0.0;
+		for(int x=-1; x<=1; x++){
+			for(int y=-1; y<=1; y++){
+				ivec2 off = ivec2(x,y);
+				result += texture2DCompare(depths, uv, off, compare);
+			}
+		}
+		return result/9.0;
 	}
 	
 	float VSM(sampler2D depths, vec2 uv, float compare)
@@ -33,16 +68,14 @@ common_fragment = createShaderSource([[
 			return normal;
 		#endif
 	}
-		
-		
 						
-	float getShadowmapValue(
-		vec4 position,
-		mat4 shadowmap_matrix0,
-		mat4 shadowmap_matrix1, 
-		mat4 shadowmap_matrix2, 
-		mat4 shadowmap_matrix3, 
-		sampler2D shadowmap)
+						
+	uniform mat4 shadowmap_matrix0;
+	uniform mat4 shadowmap_matrix1;
+	uniform mat4 shadowmap_matrix2;
+	uniform mat4 shadowmap_matrix3;
+	uniform sampler2D	shadowmap;
+	float getShadowmapValue(vec4 position)
 	{
 		#ifdef SHADOW_RECEIVER
 			vec3 shadow_coord[4] = vec3[](
@@ -100,14 +133,9 @@ base_fragment_shader = common_fragment .. createShaderSource([[
 		}
 
 	#else
-		uniform sampler2D	shadowmap;
 		#ifdef NORMAL_MAPPING
 			uniform sampler2D	normalmap;
 		#endif
-		uniform mat4 shadowmap_matrix0;
-		uniform mat4 shadowmap_matrix1;
-		uniform mat4 shadowmap_matrix2;
-		uniform mat4 shadowmap_matrix3;
 		uniform mat4 view_matrix;
 		uniform vec3 light_dir;
 		uniform vec4 ambient_color;
@@ -115,7 +143,6 @@ base_fragment_shader = common_fragment .. createShaderSource([[
 		uniform vec4 diffuse_color;
 		uniform float diffuse_intensity;
 		uniform vec4 fog_color;
-		uniform float fog_density;
 		#ifdef POINT_LIGHT_PASS
 			uniform float light_fov;
 			uniform vec3 light_pos;
@@ -148,14 +175,14 @@ base_fragment_shader = common_fragment .. createShaderSource([[
 				shadow = 1;
 			#else
 				attenuation = 1;
-				shadow = getShadowmapValue(position, shadowmap_matrix0, shadowmap_matrix1, shadowmap_matrix2, shadowmap_matrix3, shadowmap);
+				shadow = getShadowmapValue(position);
 				to_light_dir = light_dir; 
 				vec4 ambient = ambient_intensity * surface * ambient_color;
 			#endif // if POINT_LIGHT_PASS else
 
 			shadow *=  max(0.0, dot(normal, -to_light_dir));
 			vec4 diffuse = shadow * diffuse_intensity * surface * diffuse_color;
-			out_color = mix(attenuation * (diffuse + ambient), fog_color, getFogFactor(p.z / p.w, fog_density));
+			out_color = mix(attenuation * (diffuse + ambient), fog_color, getFogFactor(p.z / p.w));
 		}
 		
 	#endif // if SHADOW_PASS else

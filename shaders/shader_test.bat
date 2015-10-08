38a6c04316ffa64bf472bf9d9fbba090d9d36dd5
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-10-08 21:47:25 +0200

    shader test

---
+++ shaders/shader_test.bat
@@ -0,0 +1,6 @@
+ rmdir /Q compiled
+ del /Q shader_test.log
+ mkdir compiled
+ cd..
+ "shaders/shaderc.exe" -f shaders/rigid_fs.sc -o shaders/compiled/rigid_MAIN0_fs.shb --depends --platform windows --type fragment --profile ps_4_0 -D MAIN > shaders/shader_test.log 2>&1
+ 

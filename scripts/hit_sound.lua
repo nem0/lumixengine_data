e2a2f0de88032f4d87402070aeb672e7ad08d9d0
Mikulas Florek <mikulas.florek@gamedev.sk>
2015-12-09 22:11:56 +0100

    new executables - source code 77e566f45f48b1a02ac66e8d92d313f469b772c8

--- scripts/hit_sound.lua
+++ scripts/hit_sound.lua
@@ -1,6 +1,6 @@
 -- LUMIX PROPERTY VOLUME float
 
 function onContact(entity)
-	local s = API_playSound(g_scene_audio, this, "hit_sound", false)
-	API_setSoundVolume(g_scene_audio, s, VOLUME)
+	local s = Audio.playSound(g_scene_audio, this, "hit_sound", false)
+	Audio.setSoundVolume(g_scene_audio, s, VOLUME)
 end
--- scripts/player.lua
+++ scripts/player.lua
@@ -2,7 +2,7 @@
 -- LUMIX PROPERTY PLAYER_SPEED float
 -- LUMIX PROPERTY MOUSE_SENSITIVITY float
 
-cmp = API_createComponent(API_getScene(g_universe_context, "physics"), "physical_controller", this)
+cmp = Engine.createComponent(g_scene_physics, "physical_controller", this)
 
 local LSHIFT_KEY = 160
 
@@ -22,25 +22,36 @@ local CONTROLLER_MOVE_Y = 10
 local yaw = 0
 local pitch = 0
 
-API_addInputAction(g_engine, LEFT_ACTION, 0, string.byte("A"), -1)
-API_addInputAction(g_engine, RIGHT_ACTION, 0, string.byte("D"), -1)
-API_addInputAction(g_engine, FORWARD_ACTION, 0, string.byte("W"), -1)
-API_addInputAction(g_engine, BACK_ACTION, 0, string.byte("S"), -1)
-API_addInputAction(g_engine, ROT_H_ACTION, 2, 0, -1)
-API_addInputAction(g_engine, ROT_V_ACTION, 3, 0, -1)
-API_addInputAction(g_engine, SPRINT_ACTION, 0, LSHIFT_KEY, -1)
+Engine.addInputAction(g_engine, LEFT_ACTION, 0, string.byte("A"), -1)
+Engine.addInputAction(g_engine, RIGHT_ACTION, 0, string.byte("D"), -1)
+Engine.addInputAction(g_engine, FORWARD_ACTION, 0, string.byte("W"), -1)
+Engine.addInputAction(g_engine, BACK_ACTION, 0, string.byte("S"), -1)
+Engine.addInputAction(g_engine, ROT_H_ACTION, 2, 0, -1)
+Engine.addInputAction(g_engine, ROT_V_ACTION, 3, 0, -1)
+Engine.addInputAction(g_engine, SPRINT_ACTION, 0, LSHIFT_KEY, -1)
 
-API_addInputAction(g_engine, CONTROLLER_ROT_X, 6, 0, 0)
-API_addInputAction(g_engine, CONTROLLER_ROT_Y, 7, 0, 0)
-API_addInputAction(g_engine, CONTROLLER_MOVE_X, 4, 0, 0)
-API_addInputAction(g_engine, CONTROLLER_MOVE_Y, 5, 0, 0)
+Engine.addInputAction(g_engine, CONTROLLER_ROT_X, 6, 0, 0)
+Engine.addInputAction(g_engine, CONTROLLER_ROT_Y, 7, 0, 0)
+Engine.addInputAction(g_engine, CONTROLLER_MOVE_X, 4, 0, 0)
+Engine.addInputAction(g_engine, CONTROLLER_MOVE_Y, 5, 0, 0)
+
+--local s = API_playSound(g_scene_audio, this, "test", false)
+--API_setEcho(g_scene_audio, s, 50, 50, 500, 500)
+
+local ETA = 0
 
 function update(dt)
-	yaw = yaw + API_getInputActionValue(g_engine, ROT_H_ACTION) * -0.01 * MOUSE_SENSITIVITY;
-	pitch = pitch + API_getInputActionValue(g_engine, ROT_V_ACTION) * -0.01 * MOUSE_SENSITIVITY;
+		ETA = ETA - dt
+		if ETA < 0 then
+			ETA = 2.0
+			local s = Audio.playSound(g_scene_audio, this, "test", false)
+		end
+
+	yaw = yaw + Engine.getInputActionValue(g_engine, ROT_H_ACTION) * -0.01 * MOUSE_SENSITIVITY;
+	pitch = pitch + Engine.getInputActionValue(g_engine, ROT_V_ACTION) * -0.01 * MOUSE_SENSITIVITY;
 	
-	yaw = yaw + API_getInputActionValue(g_engine, CONTROLLER_ROT_X) * -0.03 * MOUSE_SENSITIVITY;
-	pitch = pitch + API_getInputActionValue(g_engine, CONTROLLER_ROT_Y) * 0.03 * MOUSE_SENSITIVITY;
+	yaw = yaw + Engine.getInputActionValue(g_engine, CONTROLLER_ROT_X) * -0.03 * MOUSE_SENSITIVITY;
+	pitch = pitch + Engine.getInputActionValue(g_engine, CONTROLLER_ROT_Y) * 0.03 * MOUSE_SENSITIVITY;
 
 	local PITCH_LIMIT = 0.7
 	
@@ -49,36 +60,36 @@ function update(dt)
 	
 	local speed = PLAYER_SPEED
 	
-	if API_getInputActionValue(g_engine, SPRINT_ACTION) > 0 then
+	if Engine.getInputActionValue(g_engine, SPRINT_ACTION) > 0 then
 		speed = speed * 3
 	end
 	
-	API_setEntityLocalRotation(g_universe_context, CAMERA_ENTITY, 1, 0, 0, pitch)
+	Engine.setEntityLocalRotation(g_scene_hierarchy, CAMERA_ENTITY, 1, 0, 0, pitch)
 	
-	API_setEntityRotation(g_universe, this, 0, 1, 0, yaw);
+	Engine.setEntityRotation(g_universe, this, 0, 1, 0, yaw);
 
-	local scene = API_getScene(g_universe_context, "physics");
+	local scene = g_scene_physics;
 
-	local v = API_multVecQuat(API_getInputActionValue(g_engine, CONTROLLER_MOVE_X) * 0.1, 0, 0, 0, 1, 0, yaw)
-	API_moveController(scene, cmp, v[0], v[1], v[2], dt)
+	local v = Engine.multVecQuat(Engine.getInputActionValue(g_engine, CONTROLLER_MOVE_X) * 0.1, 0, 0, 0, 1, 0, yaw)
+	Physics.moveController(scene, cmp, v[0], v[1], v[2], dt)
 
-	local v = API_multVecQuat(0, 0, -API_getInputActionValue(g_engine, CONTROLLER_MOVE_Y) * 0.1, 0, 1, 0, yaw)
-	API_moveController(scene, cmp, v[0], v[1], v[2], dt)
+	local v = Engine.multVecQuat(0, 0, -Engine.getInputActionValue(g_engine, CONTROLLER_MOVE_Y) * 0.1, 0, 1, 0, yaw)
+	Physics.moveController(scene, cmp, v[0], v[1], v[2], dt)
 	
-	if API_getInputActionValue(g_engine, LEFT_ACTION) > 0 then
-		local v = API_multVecQuat(-speed, 0, 0, 0, 1, 0, yaw)
-		API_moveController(scene, cmp, v[0], v[1], v[2], dt)
+	if Engine.getInputActionValue(g_engine, LEFT_ACTION) > 0 then
+		local v = Engine.multVecQuat(-speed, 0, 0, 0, 1, 0, yaw)
+		Physics.moveController(scene, cmp, v[0], v[1], v[2], dt)
 	end
-	if API_getInputActionValue(g_engine, RIGHT_ACTION) > 0 then
-		local v = API_multVecQuat(speed, 0, 0, 0, 1, 0, yaw)
-		API_moveController(scene, cmp, v[0], v[1], v[2], dt)
+	if Engine.getInputActionValue(g_engine, RIGHT_ACTION) > 0 then
+		local v = Engine.multVecQuat(speed, 0, 0, 0, 1, 0, yaw)
+		Physics.moveController(scene, cmp, v[0], v[1], v[2], dt)
 	end
-	if API_getInputActionValue(g_engine, FORWARD_ACTION) > 0 then
-		local v = API_multVecQuat(0, 0, -speed, 0, 1, 0, yaw)
-		API_moveController(scene, cmp, v[0], v[1], v[2], dt)
+	if Engine.getInputActionValue(g_engine, FORWARD_ACTION) > 0 then
+		local v = Engine.multVecQuat(0, 0, -speed, 0, 1, 0, yaw)
+		Physics.moveController(scene, cmp, v[0], v[1], v[2], dt)
 	end
-	if API_getInputActionValue(g_engine, BACK_ACTION) > 0 then
-		local v = API_multVecQuat(0, 0, speed, 0, 1, 0, yaw)
-		API_moveController(scene, cmp, v[0], v[1], v[2], dt)
+	if Engine.getInputActionValue(g_engine, BACK_ACTION) > 0 then
+		local v = Engine.multVecQuat(0, 0, speed, 0, 1, 0, yaw)
+		Physics.moveController(scene, cmp, v[0], v[1], v[2], dt)
 	end
 end

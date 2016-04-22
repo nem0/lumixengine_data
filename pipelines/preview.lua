aaa0a28c4a5125cd60a58521a90f9a89810a797d
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-04-22 13:11:51 +0200

    billboard shader

--- pipelines/preview.lua
+++ pipelines/preview.lua
@@ -1,6 +1,6 @@
 addFramebuffer(this, "default", {
-	width = 1024,
-	height = 1024,
+	width = 512,
+	height = 512,
 	renderbuffers = {
 		{ format = "rgba8" },
 		{ format = "depth24" }
@@ -12,7 +12,7 @@ function render()
 		setPass(this, "MAIN")
 		enableDepthWrite(this)
 		enableRGBWrite(this)
-		clear(this, CLEAR_COLOR | CLEAR_DEPTH, 0x00000000)
+		clear(this, CLEAR_COLOR | CLEAR_DEPTH, 0xff00ff00)
 		setFramebuffer(this, "default")
 		applyCamera(this, "main")
 		setActiveGlobalLightUniforms(this)

1e36ea500bae193c8c4d50c457b7b519c452326d
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-05-26 21:10:07 +0200

    fixed shaders on windows

--- shaders/lumix_common.sh
+++ shaders/lumix_common.sh
@@ -299,7 +299,7 @@ float directionalLightShadow(sampler2D shadowmap, mat4 shadowmapMatrices[4], vec
 	else
 		return 1.0;
 
-	const float offsets[5] = float[](0.0000009, 0.000005, 0.00001, 0.00005, -1.0); // for distances 6, 14, 40, 100
+	const float offsets[5] = {0.0000009, 0.000005, 0.00001, 0.00005, -1.0}; // for distances 6, 14, 40, 100
 	float nl_tan = tan(acos(ndotl));
 	float bias = clamp(offsets[split_index]*nl_tan, 0.0, 0.1); 
 	float next_bias = clamp(offsets[split_index+1]*nl_tan, 0.0, 0.1); 

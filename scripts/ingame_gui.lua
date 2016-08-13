c93fea01a6a8dc91e36bdb06d0139e68bb10ef75
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-08-13 10:08:39 +0200

    new executables 581796a2cd2e3e3f6c4b4a617cdff148d53bc3f0

--- scripts/ingame_gui.lua
+++ scripts/ingame_gui.lua
@@ -1,12 +1,35 @@
-Gui.loadFile(Gui.instance, "gui/button.tb.txt")
-
 local MENU_ACTION = 125
-Engine.addInputAction(g_engine, MENU_ACTION, Engine.INPUT_TYPE_DOWN, string.byte("M"), -1)
+player_entity = -1
+Editor.setPropertyType("player_entity", Editor.ENTITY_PROPERTY)
+
+function toggleGUI()
+	local is_gui = Gui.isGUIShown(Gui.instance)
+	local player_env = LuaScript.getEnvironment(g_scene_lua_script, player_entity, 0)
+	if player_env then
+		player_env.input_enabled = is_gui
+	end
+	Gui.showGUI(Gui.instance, not is_gui)
+end
 
 function update()
 	if Engine.getInputActionValue(g_engine, MENU_ACTION) > 0 then
-		Gui.showGUI(Gui.instance, not Gui.isGUIShown(Gui.instance))
+		toggleGUI()
 	end
+end
+
+function onContinue()
+	toggleGUI()
+end
+
+function onExit()
+	if Editor.editor then
+		Gui.showGUI(Gui.instance, false)
+		Editor.exitGameMode(Editor.editor)
+	end
+end
+
+function onOptions()
+	Gui.loadFile(Gui.instance, "gui/options.tb.txt")
 end
 
 function onGUI()
@@ -17,3 +40,24 @@ function onGUI()
 		Gui.loadFile(Gui.instance, "gui/button.tb.txt")
 	end
 end
+
+function onDestroy()
+	if Gui.isGUIShown(Gui.instance) then toggleGUI() end
+	Gui.unregisterEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "continue")
+	Gui.unregisterEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "exit")
+	Gui.unregisterEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "options")
+	Gui.unregisterEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "options_back")
+end
+
+Gui.loadFile(Gui.instance, "gui/button.tb.txt")
+Gui.showGUI(Gui.instance, false)
+
+Engine.addInputAction(g_engine, MENU_ACTION, Engine.INPUT_TYPE_DOWN, string.byte("M"), -1)
+
+Gui.registerEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "continue", onContinue)
+Gui.registerEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "exit", onExit)
+Gui.registerEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "options", onOptions)
+Gui.registerEvent(Gui.instance, Gui.EVENT_TYPE_CLICK, "options_back", function () 
+	Gui.loadFile(Gui.instance, "gui/button.tb.txt")
+end)
+

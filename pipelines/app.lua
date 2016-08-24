10434719ce6aaa511bb2528632462d575df52fea
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-08-24 09:23:49 +0200

    fixed app pipeline

--- pipelines/app.lua
+++ pipelines/app.lua
@@ -1,13 +1,99 @@
 common = require "pipelines/common"
-ctx = { pipeline = this, main_framebuffer = "default" }
+ctx = { pipeline = this, main_framebuffer = "forward" }
+do_gamma_mapping = true
 
+local render_debug_deferred = { false, false, false, false }
+local render_debug_deferred_fullsize = { false, false, false, false }
+
+
+addFramebuffer(this, "forward", {
+	width = 1024,
+	height = 1024,
+	size_ratio = {1, 1},
+	renderbuffers = {
+		{ format = "rgba8" },
+		{ format = "depth24stencil8" }
+	}
+})
+
+addFramebuffer(this, "g_buffer", {
+	width = 1024,
+	height = 1024,
+	screen_size = true,
+	renderbuffers = {
+		{ format = "rgba8" },
+		{ format = "rgba8" },
+		{ format = "rgba8" },
+		{ format = "depth24stencil8" }
+	}
+})
+  
 common.init(ctx)
 common.initShadowmap(ctx)
 
 
 local texture_uniform = createUniform(this, "u_texture")
-local screen_space_material = Engine.loadResource(g_engine, "pipelines/screenspace/screenspace.mat", "material")
+local screen_space_material = Engine.loadResource(g_engine,"pipelines/screenspace/screenspace.mat", "material")
+local gbuffer0_uniform = createUniform(this, "u_gbuffer0")
+local gbuffer1_uniform = createUniform(this, "u_gbuffer1")
+local gbuffer2_uniform = createUniform(this, "u_gbuffer2")
+local gbuffer_depth_uniform = createUniform(this, "u_gbuffer_depth")
+local deferred_material = Engine.loadResource(g_engine,"pipelines/common/deferred.mat", "material")
+local deferred_point_light_material = Engine.loadResource(g_engine,"pipelines/common/deferredpointlight.mat", "material")
+local gamma_mapping_material = Engine.loadResource(g_engine,"pipelines/common/gamma_mapping.mat", "material")
 
+
+function deferred(camera_slot)
+	deferred_view = newView(this, "deferred")
+		setPass(this, "DEFERRED")
+		setFramebuffer(this, "g_buffer")
+		applyCamera(this, camera_slot)
+		clear(this, CLEAR_ALL, 0x00000000)
+		
+		setStencil(this, STENCIL_OP_PASS_Z_REPLACE 
+			| STENCIL_OP_FAIL_Z_KEEP 
+			| STENCIL_OP_FAIL_S_KEEP 
+			| STENCIL_TEST_ALWAYS)
+		setStencilRMask(this, 0xff)
+		setStencilRef(this, 1)
+	
+	newView(this, "copyRenderbuffer");
+		copyRenderbuffer(this, "g_buffer", 3, ctx.main_framebuffer, 1)
+		
+	newView(this, "main")
+		setPass(this, "MAIN")
+		setFramebuffer(this, ctx.main_framebuffer)
+		applyCamera(this, camera_slot)
+		clear(this, CLEAR_COLOR | CLEAR_DEPTH, 0x00000000)
+		
+		setActiveGlobalLightUniforms(this)
+		bindFramebufferTexture(this, "g_buffer", 0, gbuffer0_uniform)
+		bindFramebufferTexture(this, "g_buffer", 1, gbuffer1_uniform)
+		bindFramebufferTexture(this, "g_buffer", 2, gbuffer2_uniform)
+		bindFramebufferTexture(this, "g_buffer", 3, gbuffer_depth_uniform)
+		drawQuad(this, 0, 0, 1, 1, deferred_material)
+
+	newView(this, "deferred_debug_shapes")
+		setPass(this, "EDITOR")
+		setFramebuffer(this, ctx.main_framebuffer)
+		applyCamera(this, camera_slot)
+		setStencil(this, STENCIL_OP_PASS_Z_REPLACE 
+			| STENCIL_OP_FAIL_Z_KEEP 
+			| STENCIL_OP_FAIL_S_KEEP 
+			| STENCIL_TEST_ALWAYS)
+		setStencilRMask(this, 0xff)
+		setStencilRef(this, 1)
+		renderDebugShapes(this)
+		
+	newView(this, "deferred_local_light")
+		setPass(this, "MAIN")
+		setFramebuffer(this, ctx.main_framebuffer)
+		disableDepthWrite(this)
+		enableBlending(this, "add")
+		applyCamera(this, camera_slot)
+		renderLightVolumes(this, deferred_point_light_material)
+		disableBlending(this)
+end
 
 function main()
 	main_view = newView(this, "MAIN")
@@ -30,7 +116,7 @@ function fur()
 		enableBlending(this, "alpha")
 		applyCamera(this, "main")
 		setActiveGlobalLightUniforms(this)
-		renderModels(this, {main_view, fur_view})
+		renderModels(this, {deferred_view, fur_view})
 end
 
 
@@ -44,16 +130,29 @@ function pointLight()
 		renderPointLightLitGeometry(this)
 end
 
+function ingameGUI()
+	newView(this, "ingame_gui")
+		setPass(this, "IMGUI")
+		setFramebuffer(this, "default")
+		clear(this, CLEAR_DEPTH, 0x303030ff)
+		renderIngameGUI(this)
+end
 
 function render()
 	common.shadowmap(ctx, "main")
-	main(this)
+	deferred("main")
 	common.particles(ctx, "main")
-	pointLight(this)		
 	fur(this)
 
-	if not postprocessCallback(this, "main") then
-		-- todo SRGB
-	end
-end
+	postprocessCallback(this, "main")
 
+	if do_gamma_mapping then
+		newView(this, "SRGB")
+			setPass(this, "GAMMA_MAPPING")
+			setFramebuffer(this, "default")
+			bindFramebufferTexture(this, "forward", 0, texture_uniform)
+			drawQuad(this, 0, 0, 1, 1, gamma_mapping_material)
+	end
+	
+	ingameGUI(ctx)
+end

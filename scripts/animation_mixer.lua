4a9699a90b0c1cfafb20667b8ad1315c46ce295e
Mikulas Florek <mikulas.florek@gamedev.sk>
2016-08-25 12:38:11 +0200

    new executables de84926c9d7b9d552238a12d89012271369aba60 animation mixer test script

--- scripts/animation_mixer.lua
+++ scripts/animation_mixer.lua
@@ -13,9 +13,9 @@ function createState(anim)
 	state.update = function (time_delta, state)
 		state.time = state.time + time_delta * 30
 		for _, out in ipairs(state.out) do
-			if out.canGo(out) then return out end
+			if out.canGo(out) then return out, true end
 		end
-		return state
+		return state, false
 	end
 	state.mix = function(mixer, state, slot, weight)
 		Animation.mixAnimation(g_scene_animation, mixer, state.anim, slot, state.time, weight)
@@ -23,6 +23,7 @@ function createState(anim)
 	end
 	return state
 end
+
 
 function createTransition(from, to, length)
 	local trans = {}
@@ -32,20 +33,23 @@ function createTransition(from, to, length)
 	trans.to = to
 	trans.time = 0
 	trans.canGo = function (trans)
-		return trans.from.time > 30
+		return trans.from.time > 60
 	end
 	trans.enter = function (trans)
 		trans.time = 0
+		trans.to.enter(trans.to)
 	end
 	trans.update = function (time_delta, trans)
 		trans.time = trans.time + time_delta
 		trans.from.update(time_delta, trans.from)
 		trans.to.update(time_delta, trans.to)
-		if trans.time > trans.length then return trans.to end
-		return trans
+		if trans.time > trans.length then return trans.to, false end
+		return trans, false
 	end
 	trans.mix = function(mixer, trans, slot, weight)
-		local w1 = weight * (trans.time / trans.length) 
+		local t = trans.time / trans.length
+		if t > 1 then t = 1 end
+		local w1 = weight * t
 		local w0 = weight - w1
 		local next_slot = trans.from.mix(mixer, trans.from, slot, w0)
 		return trans.to.mix(mixer, trans.to, next_slot, w1)
@@ -53,25 +57,28 @@ function createTransition(from, to, length)
 	return trans
 end
 
+
 function createSimpleState(path)
 	local anim = Engine.loadResource(g_engine, path, "animation")
 	return createState(anim)
 end
 
+
 local state0 = createSimpleState("models/creatures/deer/run.ani")
 local state1 = createSimpleState("models/creatures/deer/deer1Armature_Eat_001.ani")
 local state2 = createSimpleState("models/creatures/deer/deer1Armature_LookAround_000.ani")
-local transition0 = createTransition(state0, state1, 1.0)
-local transition1 = createTransition(state1, state2, 1.0)
-local transition2 = createTransition(state2, state0, 1.0)
+local transition0 = createTransition(state0, state1, 0.3)
+local transition1 = createTransition(state1, state2, 0.3)
+local transition2 = createTransition(state2, state0, 0.1)
 local current = state0
+
 
 state0.name = "state0"
 state1.name = "state1"
 state2.name = "state2"
 transition0.name = "trans0"
 transition1.name = "trans1"
-transition1.name = "trans2"
+transition2.name = "trans2"
 
 
 function onGUI()
@@ -86,12 +93,8 @@ end
 
 
 function update(time_delta)
-	local mixer = Engine.getComponent(g_universe, this, MIXER_TYPE)
-	if mixer < 0 then return end
-
 	local old = current
-	current = current.update(time_delta, current)
-	if old ~= current then current.enter(current) end
-	slot = current.mix(mixer, current, 0, 1.0)
-	Animation.mixAnimation(g_scene_animation, mixer, -1, slot, 0, 0)
+	current, call_enter = current.update(time_delta, current)
+	if call_enter then current.enter(current) end
+	slot = current.mix(this, current, 0, 1.0)
 end

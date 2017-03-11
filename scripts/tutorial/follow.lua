56454cbe8416a627bc9367449fba39c6e7bdc5c6
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-03-11 13:05:32 +0100

    tutorial comments

--- scripts/tutorial/follow.lua
+++ scripts/tutorial/follow.lua
@@ -8,24 +8,30 @@ local attack_input_idx = -1
 local next_follow = 2
 
 function init()
+	-- cache some stuff
 	anim_ctrl = Engine.getComponent(g_universe, this, ANIM_CONTROLLER_TYPE)
 	speed_input_idx = Animation.getControllerInputIndex(g_scene_animation, anim_ctrl, "speed")
 	attack_input_idx = Animation.getControllerInputIndex(g_scene_animation, anim_ctrl, "attack")
 end
 
 function onPathFinished()
+	-- when the enemy finish walking, play attack animation
 	Animation.setControllerBoolInput(g_scene_animation, anim_ctrl, attack_input_idx, true)
 end
 
 function update(time_delta)
+	-- get agent speed from navigation and set it as input to animation controller
+	-- so it can play the right animation
 	local agent_speed = Navigation.getAgentSpeed(g_scene_navigation, this)
 	Animation.setControllerFloatInput(g_scene_animation, anim_ctrl, speed_input_idx, agent_speed)
 
+    -- try to move to where player is every few seconds
 	next_follow = next_follow - time_delta
 	if next_follow > 0 then return end
 	
-	Engine.logError("follow")
 	next_follow = 5
+	-- get player's position
 	local pos = Engine.getEntityPosition(g_universe, followed_entity)
+	-- move to where player is
 	Navigation.navigate(g_scene_navigation, this, pos, speed)
 end

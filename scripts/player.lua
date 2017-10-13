e84cd7d18676f18e7eeb90b67163861909c2cff5
Mikulas Florek <mikulas.florek@gamedev.sk>
2017-10-13 23:46:34 +0200

    new executables

--- scripts/player.lua
+++ scripts/player.lua
@@ -6,14 +6,6 @@ input_enabled = true
 
 local LSHIFT_KEY = 160
 
-local LEFT_ACTION = 0
-local RIGHT_ACTION = 1
-local FORWARD_ACTION = 2
-local BACK_ACTION = 3
-local ROT_H_ACTION = 4
-local SPRINT_ACTION = 5
-local ROT_V_ACTION = 6
-
 local CONTROLLER_ROT_X = 7
 local CONTROLLER_ROT_Y = 8
 local CONTROLLER_MOVE_X = 9
@@ -33,33 +25,44 @@ function makeQuat(axis, angle)
 	}
 end
 
+local actions = {
+	forward = false,
+	left = false,
+	right = false,
+	back = false,
+	sprint = false,
+	rot_h = 0,
+	rot_v = 0
+}
+function onInputEvent(event)
+	if event.type == Engine.INPUT_EVENT_BUTTON then
+		if event.device.type == Engine.INPUT_DEVICE_KEYBOARD then
+			if event.key_id == 119 then
+				actions.forward = event.state ~= 0
+			elseif event.key_id == 97 then
+				actions.left = event.state ~= 0
+			elseif event.key_id == 100 then
+				actions.right = event.state ~= 0
+			elseif event.key_id == 115 then
+				actions.back = event.state ~= 0
+			elseif event.key_id == 1073742049 then
+				actions.sprint = event.state ~= 0
+			end
+		end		
+	elseif event.type == Engine.INPUT_EVENT_AXIS then
+		if event.device.type == Engine.INPUT_DEVICE_MOUSE then
+			yaw = yaw + event.x * -0.005 * MOUSE_SENSITIVITY;
+			pitch = pitch + event.y * -0.005 * MOUSE_SENSITIVITY;
+		end
+	end
+end
 
 function init()
 	cmp = Engine.createComponent(g_universe, this, "physical_controller")
-
-	Engine.addInputAction(g_engine, LEFT_ACTION, Engine.INPUT_TYPE_PRESSED, string.byte("A"), -1)
-	Engine.addInputAction(g_engine, RIGHT_ACTION, Engine.INPUT_TYPE_PRESSED, string.byte("D"), -1)
-	Engine.addInputAction(g_engine, FORWARD_ACTION, Engine.INPUT_TYPE_PRESSED, string.byte("W"), -1)
-	Engine.addInputAction(g_engine, BACK_ACTION, Engine.INPUT_TYPE_PRESSED, string.byte("S"), -1)
-	Engine.addInputAction(g_engine, ROT_H_ACTION, Engine.INPUT_TYPE_MOUSE_X, 0, -1)
-	Engine.addInputAction(g_engine, ROT_V_ACTION, Engine.INPUT_TYPE_MOUSE_Y, 0, -1)
-	Engine.addInputAction(g_engine, SPRINT_ACTION, Engine.INPUT_TYPE_PRESSED, LSHIFT_KEY, -1)
-
-	Engine.addInputAction(g_engine, CONTROLLER_ROT_X, Engine.INPUT_TYPE_RTHUMB_X, 0, 0)
-	Engine.addInputAction(g_engine, CONTROLLER_ROT_Y, Engine.INPUT_TYPE_RTHUMB_Y, 0, 0)
-	Engine.addInputAction(g_engine, CONTROLLER_MOVE_X, Engine.INPUT_TYPE_LTHUMB_X, 0, 0)
-	Engine.addInputAction(g_engine, CONTROLLER_MOVE_Y, Engine.INPUT_TYPE_LTHUMB_Y, 0, 0)
 end
 
 function update(dt)
 	if not input_enabled then return end
-
-	yaw = yaw + Engine.getInputActionValue(g_engine, ROT_H_ACTION) * -0.01 * MOUSE_SENSITIVITY;
-	pitch = pitch + Engine.getInputActionValue(g_engine, ROT_V_ACTION) * -0.01 * MOUSE_SENSITIVITY;
-	
-	yaw = yaw + Engine.getInputActionValue(g_engine, CONTROLLER_ROT_X) * -0.03 * MOUSE_SENSITIVITY;
-	pitch = pitch + Engine.getInputActionValue(g_engine, CONTROLLER_ROT_Y) * 0.03 * MOUSE_SENSITIVITY;
-
 	local PITCH_LIMIT = 0.7
 	
 	if pitch > PITCH_LIMIT then pitch = PITCH_LIMIT end
@@ -67,7 +70,7 @@ function update(dt)
 	
 	local speed = PLAYER_SPEED
 	
-	if Engine.getInputActionValue(g_engine, SPRINT_ACTION) > 0 then
+	if actions.sprint then
 		speed = speed * 3
 	end
 	
@@ -77,25 +80,19 @@ function update(dt)
 
 	local scene = g_scene_physics;
 
-	local v = Engine.multVecQuat({Engine.getInputActionValue(g_engine, CONTROLLER_MOVE_X) * 0.1, 0, 0}, {0, 1, 0}, yaw)
-	Physics.moveController(scene, cmp, v, dt)
-
-	local v = Engine.multVecQuat({0, 0, -Engine.getInputActionValue(g_engine, CONTROLLER_MOVE_Y) * 0.1}, {0, 1, 0}, yaw)
-	Physics.moveController(scene, cmp, v, dt)
-	
-	if Engine.getInputActionValue(g_engine, LEFT_ACTION) > 0 then
+	if actions.left then
 		local v = Engine.multVecQuat({-speed, 0, 0}, {0, 1, 0}, yaw)
 		Physics.moveController(scene, cmp, v, dt)
 	end
-	if Engine.getInputActionValue(g_engine, RIGHT_ACTION) > 0 then
+	if actions.right then
 		local v = Engine.multVecQuat({speed, 0, 0}, {0, 1, 0}, yaw)
 		Physics.moveController(scene, cmp, v, dt)
 	end
-	if Engine.getInputActionValue(g_engine, FORWARD_ACTION) > 0 then
+	if actions.forward then
 		local v = Engine.multVecQuat({0, 0, -speed}, {0, 1, 0}, yaw)
 		Physics.moveController(scene, cmp, v, dt)
 	end
-	if Engine.getInputActionValue(g_engine, BACK_ACTION) > 0 then
+	if actions.back then
 		local v = Engine.multVecQuat({0, 0, speed}, {0, 1, 0}, yaw)
 		Physics.moveController(scene, cmp, v, dt)
 	end

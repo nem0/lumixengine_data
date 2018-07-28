d922a2949eef266db91d4ba57882f9a4000aa82d
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-07-28 14:51:46 +0200

    ffr

--- pipelines/common.sh
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright 2011-2015 Branimir Karadzic. All rights reserved.
- * License: http://www.opensource.org/licenses/BSD-2-Clause
- */
-
-#include "common/bgfx_shader.sh"
-#include "common/lumix_common.sh"

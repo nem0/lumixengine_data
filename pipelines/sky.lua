7b72e3bbc5929d663aab93d2294282da1872fec9
Mikulas Florek <mikulas.florek@gamedev.sk>
2018-09-08 14:40:55 +0200

    ffr WIP

--- pipelines/sky.lua
+++ pipelines/sky.lua
@@ -1,15 +1,13 @@
-local procedural_sky_shader = nil
-
 function postprocess(env, transparent_phase, hdr_buffer, gbuffer0, gbuffer1, gbuffer_depth, shadowmap)
 	if not enabled then return hdr_buffer end
 	if transparent_phase ~= "pre" then return hdr_buffer end
 	env.beginBlock("sky")
-	if procedural_sky_shader == nil then
-		procedural_sky_shader = env.preloadShader("pipelines/procedural_sky.shd")
+	if env.procedural_sky_shader == nil then
+		env.procedural_sky_shader = env.preloadShader("pipelines/procedural_sky.shd")
 	end
 	env.setRenderTargets(0, hdr_buffer, gbuffer_depth)
 	env.setStencil(0x0, env.STENCIL_NOT_EQUAL, 1, 0xff, env.STENCIL_KEEP, env.STENCIL_KEEP, env.STENCIL_REPLACE)
-	env.drawArray(0, 4, procedural_sky_shader)
+	env.drawArray(0, 4, env.procedural_sky_shader)
 	env.setStencil(0xff, env.STENCIL_DISABLE, 1, 0xff, env.STENCIL_KEEP, env.STENCIL_KEEP, env.STENCIL_KEEP)
 	env.endBlock()
 	return hdr_buffer
